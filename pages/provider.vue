4bbf5558e10b1fc96a58074588f66e979746fec2
HamidNE <hamidne@mail.ru>
2019-01-29 22:37:12 +0330

    edit for providers page card style and fixed layout

--- pages/provider.vue
+++ pages/provider.vue
@@ -1,14 +1,45 @@
 <template>
-  <div>
+  <div class="columns is-multiline">
+    <div class="column is-one-third-desktop provider-card">
+      <img src="https://media.licdn.com/dms/image/C5603AQHei-zk5_GcZA/profile-displayphoto-shrink_800_800/0?e=1554336000&v=beta&t=CzA4yePkQ_AkBnTj9mXxx9GghNsNP375ztA12WK6G1Q">
+      <h3 class="title">امیر مقیمی</h3>
+      <p>
+        I am an experienced Software Engineer, and team builder, passionate about design, development and operations of highly scalable software systems based on microservices in the cloud. Although I'm deeply technical, I'm a lot more passionate about the results of our work and its impact on our customer's lives.
+      </p>
+      <ul class="socials">
+        <li><a href="https://stackoverflow.com/users/3634398/alex-kyriakidis" target="_blank"
+               title="Alex Kyriakidis on Stack Overflow"><i class="fab fa-stack-overflow"></i></a></li>
+        <li><a href="https://twitter.com/hootlex" target="_blank" title="Alex Kyriakidis on Twitter"><i
+          class="fab fa-twitter"></i></a></li>
+        <li><a href="https://github.com/hootlex" target="_blank" title="Alex Kyriakidis on Github"><i
+          class="fab fa-github"></i></a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-	export default {
-		name: "provider"
-	}
+  export default {
+    name: "provider"
+  }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .provider-card {
+    display: flex;
+    border-radius: .5rem;
+    margin-top: 11rem;
+    padding-top: 5rem;
+    position: relative;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    box-shadow: 2px 2px 15px 0 rgba(36, 37, 38, 0.08);
+    > img {
+      top: -8rem;
+      width: 40%;
+      position: absolute;
+      border-radius: 50%;
+    }
+  }
 </style>
