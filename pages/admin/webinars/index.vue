379ff356b55ed6cdc2ab1961a2291974d9567a2c
HamidNE <hamidne@mail.ru>
2019-02-14 22:13:43 +0330

    load webinars from store and edit for create webinar page

--- pages/admin/webinars/index.vue
+++ pages/admin/webinars/index.vue
@@ -2,14 +2,18 @@
   <div class="container mt-3">
     <div class="d-flex justify-content-between">
       <h3>لیست وبینار ها</h3>
-      <div class="btn btn-success">ایجاد وبینار</div>
+      <nuxt-link
+        class="btn btn-success"
+        to="/admin/webinars/create">
+        ایجاد وبینار
+      </nuxt-link>
     </div>
     <div class="row mt-3">
-      <div class="col-md-4 mb-3" v-for="(item, id) in items">
+      <div class="col-md-4 mb-3" v-for="webinar in webinars.all">
         <card
-          :title="item.title" :provider="item.provider"
-          :image-path="item.baner" :href="'/admin/webinars/' + id">
-          {{ item.description }}
+          :title="webinar.title" :provider="webinar.provider"
+          :image-path="webinar.baner" :href="'/admin/webinars/' + webinar.id">
+          {{ webinar.description }}
         </card>
       </div>
     </div>
@@ -17,15 +21,13 @@
 </template>
 
 <script>
+  import {mapState} from 'vuex';
 	import Card from "../../../components/Card";
   export default {
 		name: "index",
     components: {Card},
     layout: 'admin',
-    async asyncData ({ params, app }) {
-      let { data } = await app.$axios.get('webinars.json');
-      return { items: data }
-    }
+    computed: mapState(['webinars'])
 	}
 </script>
 
