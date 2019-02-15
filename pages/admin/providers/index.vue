ca32bfa25923bee0fb7006499944ea7aaeb24054
HamidNE <hamidne@mail.ru>
2019-02-15 06:16:02 +0330

    add providers index page in admin

---
+++ pages/admin/providers/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container mt-3">
+    <div class="d-flex justify-content-between">
+      <h3>لیست وبینار ها</h3>
+      <nuxt-link
+        class="btn btn-success"
+        to="/admin/webinars/create">
+        ایجاد وبینار
+      </nuxt-link>
+    </div>
+    <div class="row mt-3">
+      <div class="col-md-4 mb-3" v-for="webinar in webinars.all">
+        <card
+          :title="webinar.title" :provider="webinar.provider"
+          :image-path="webinar.baner" :href="'/admin/webinars/' + webinar.id">
+          {{ webinar.description }}
+        </card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "index",
+    layout: 'admin'
+	}
+</script>
+
+<style scoped>
+
+</style>
