9fb9e4edbcc179842cb0a7d160b1e7ae9997806b
HamidNE <hamidne@mail.ru>
2019-02-15 09:08:34 +0330

    edit for providers store file and cleanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: {color: '#fff'},
+  loading: {color: '#34bfa3'},
 
   /*
   ** Global CSS
--- store/providers.js
+++ store/providers.js
@@ -1,27 +1,3 @@
-const items = [
-  {
-    name: 'امیر مقیمی',
-    image: '/img/providers/amir-moghimi.jpeg',
-    description: 'I am an experienced Software Engineer, and team builder, passionate about design, development and operations of highly scalable software systems based on microservices in the cloud. Although I\'m deeply technical, I\'m a lot more passionate about the results of our work and its impact on our customer\'s lives.'
-  },
-  {
-    name: 'صادق علی اکبری',
-    image: '/img/providers/sadeq-ali-akbari.png',
-    description: 'صادق علی‌اکبری دانشجوی دکترای دانشگاه صنعتی شريف در رشته مهندسی کامپيوتر گرايش نرم‌افزار است. دوره کارشناسی ارشد را در دانشگاه شريف و دوره کارشناسی را در دانشگاه تهران گذرانده است. \n' +
-      'صادق سابقه تدريس درس برنامه‌نويسی پيشرفته به زبان جاوا در دانشگاه شريف (از سال 89 تا به حال)، برنامه‌نويسی وب در دانشگاه شهيد بهشتی (يک ترم) و درس برنامه‌نويسی در دبيرستان علامه حلی (4 سال) را دارد. همچنين سابقه حدود 9 سال کار به صورت برنامه‌نويس، طراح و معمار در شرکت‌های نرم‌افزاری و عموماً در حوزه جاوا را دارد.'
-  },
-  {
-    name: 'سید احسان محمودی',
-    image: '',
-    description: 'فارغ التحصیل کارشناسی مهندسی نرم افزار، کارشناسی ارشد هوش مصنوعی و رباتیک؛ دانشگاه تهران'
-  },
-  {
-    name: 'رضا یوسف زاده',
-    image: '/img/providers/reza-yousefzadeh.jpg',
-    description: 'فارغ التحصیل مهندسی نرم افزار. دانشگاه تهران'
-  },
-];
-
 export const state = () => ({
   all: []
 });
@@ -41,11 +17,9 @@ export const actions = {
     const {data} = await this.$axios.get('providers.json');
     commit('setItems', data);
   },
-  async addItem(vuexContext, item) {
-    return await this.$axios.$post('providers.json', item)
-      .then(data => {
-        vuexContext.commit("addItem", {item, id: data.name})
-      })
+  addItem(vuexContext, item) {
+    return this.$axios.$post('providers.json', item)
+      .then(data => vuexContext.commit("addItem", {item, id: data.name}))
       .catch(error => console.error(error));
   },
 };
