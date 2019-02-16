57c1779f09bd3871a52c07b06894846e5e8947f4
HamidNE <hamidne@mail.ru>
2019-02-16 21:48:43 +0330

    last commit ...

--- store/webinars.js
+++ store/webinars.js
@@ -8,6 +8,9 @@ export const mutations = {
   },
   addItem(state, item) {
     state.all[item.id] = item.item;
+  },
+  updateItem(state, item) {
+    state.all[item.id] = item.item;
   }
 };
 
@@ -15,6 +18,11 @@ export const actions = {
   async prepare({commit}) {
     const {data} = await this.$axios.get('webinars2.json');
     commit('setItems', data);
+  },
+  updateItem(vuexContext, item) {
+    return this.$axios.$post(`webinars2/${item.id}.json`, item.webinar)
+      .then(data => vuexContext.commit("updateItem", {item: item.webinar, id: item.id}))
+      .catch(error => console.error(error));
   },
   addItem(vuexContext, item) {
     return this.$axios.$post('webinars2.json', item)
