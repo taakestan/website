468efdf4e76e05711a55e3788a5705496aa73490
HamidNE <hamidne@mail.ru>
2019-02-22 20:47:22 +0330

    add index page for show admins of project

---
+++ pages/admin/admins/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mt-3">
+    <div class="d-flex justify-content-between">
+      <h3>لیست کاربران</h3>
+      <nuxt-link
+              class="btn btn-success"
+              to="/admin/admins/create">
+        اضافه کردن مدیر
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index",
+    layout: 'admin',
+  }
+</script>
+
+<style scoped>
+
+</style>
