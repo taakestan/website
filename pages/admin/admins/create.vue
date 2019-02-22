7597ac7c9e67e043d23283b5dbad82dddc2c640a
HamidNE <hamidne@mail.ru>
2019-02-22 20:52:51 +0330

    fixed create admin page

--- pages/admin/admins/create.vue
+++ pages/admin/admins/create.vue
@@ -24,15 +24,15 @@
             <form-control-feedback :errors="errors.all" field="name"/>
           </div>
           <div class="form-group col-md-4">
-            <label>نام خانوادگی</label>
+            <label>نام کاربری</label>
             <input class="form-control" v-model="admin.username"
                    :class="{'is-invalid': !!errors.all.username}">
             <form-control-feedback :errors="errors.all" field="username"/>
           </div>
           <div class="form-group col-md-4">
-            <label>نام کاربری</label>
+            <label>رمز عبور</label>
             <input class="form-control" v-model="admin.password"
-                   :class="{'is-invalid': !!errors.all.password}">
+                   :class="{'is-invalid': !!errors.all.password}" type="password">
             <form-control-feedback :errors="errors.all" field="password"/>
           </div>
         </div>
@@ -62,7 +62,7 @@
     computed: mapState(['errors']),
     methods: {
       createItem() {
-        this.$store.dispatch("providers/createItem", this.provider);
+        this.$axios.$post('/api/register', this.admin);
       },
     },
   }
