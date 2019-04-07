ef5b89c17ba7ec594c2ade43bafc69f28c9dcd9a
HamidNE <hamidne@mail.ru>
2019-04-07 17:08:13 +0430

    undo changes

--- components/WebinarCard.vue
+++ components/WebinarCard.vue
@@ -25,7 +25,6 @@
     computed: mapState(['providers']),
     methods: {
       providerName(providerID) {
-        return '';
         const provider = this.providers.all.find(item => item.id == providerID);
         return provider.first_name + ' ' + provider.last_name;
       }
