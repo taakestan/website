ecce53b5f930e20e600981f6cc07cb72a90e0741
HamidNE <hamidne@mail.ru>
2019-04-10 18:31:34 +0430

    Add nuxt-fire modules and configure settings and update .env file

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,12 +50,35 @@ module.exports = {
   ],
 
   modules: [
+    'nuxt-fire',
     '@nuxtjs/pwa',
     '@nuxtjs/axios',
     '@nuxtjs/toast',
     '@nuxtjs/dotenv',
     '@nuxtjs/sitemap'
   ],
+
+  fire: {
+    useOnly: ['auth', 'firestore', 'functions', 'storage', 'realtimeDb'],
+    config: {
+      development: {
+        apiKey: process.env.API_KEY ,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      },
+      production: {
+        apiKey: process.env.API_KEY ,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      }
+    }
+  },
 
   toast: {
     duration: 2000,
