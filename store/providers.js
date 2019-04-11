0a99529536e16b77287b056a3cbbcebd81ec95df
HamidNE <hamidne@mail.ru>
2019-04-11 22:24:54 +0430

    edit for providers store file and remove unused methods

--- store/providers.js
+++ store/providers.js
@@ -1,41 +1,18 @@
-const baseURL = '/api/providers';
 const collectionName = 'providers';
 
-export const state = () => ({all: []});
+export const state = () => ({all: Object});
 
 export const mutations = {
   setItems(state, item) {
     state.all = item;
   },
-  createItem(state, item) {
-    state.all.push(item);
-  },
-  updateItem(state, item) {
-    const index = state.all.findIndex(value => value.id === item.id);
-    state.all[index] = item;
-  },
-  deleteItem(state, id) {
-    state.all = state.all.filter(item => item.id != id);
-  },
 };
 
 export const actions = {
   async prepare(state) {
-    const data = [];
+    const data = Object();
     const response = await this.$fireStore.collection(collectionName).get();
-    await response.forEach(doc => data.push({id: doc.id, ...doc.data()}));
+    await response.forEach(doc => data[doc.id] = doc.data());
     state.commit('setItems', data);
-  },
-  createItem(state, item) {
-    this.$fireStore.collection(collectionName).add(item)
-        .then(response => state.commit("createItem", {...item, id: response.id}));
-  },
-  updateItem(state, item) {
-    return this.$axios.$put(`${baseURL}/${item.id}`, item)
-        .then(() => state.commit("updateItem", item));
-  },
-  deleteItem(state, id) {
-    return this.$axios.$delete(`${baseURL}/${id}`)
-        .then(() => state.commit('deleteItem', id))
   },
 };
