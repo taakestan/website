4f603f54d8ce3e1918205816b1a1afff99304dad
HamidNE <hamdine@mail.ru>
2019-11-07 07:08:25 +0330

    update fire config

--- config/fire.js
+++ config/fire.js
@@ -9,7 +9,9 @@ export default {
       databaseURL: process.env.DATABASE_URL,
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID
     },
     production: {
       apiKey: process.env.API_KEY,
@@ -17,7 +19,9 @@ export default {
       databaseURL: process.env.DATABASE_URL,
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID
     }
   }
 };
