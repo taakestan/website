de863d8638bb7f2ea83a2cff8c5f1f920cdfab3e
HamidNE <hamidne@mail.ru>
2019-11-08 22:41:02 +0330

    cleanup providers page content

--- pages/providers.vue
+++ pages/providers.vue
@@ -1,50 +1,50 @@
 <template>
-  <div>
-    <section class="section section--dark section--gradient section--gradient-one" id="section1">
-      <navbar/>
-      <div class="container text-center text-white pt-5">
-        <h1 class="display-3 mb-3">ارائه کنندگان</h1>
-        <p>شما هم می توانید به افراد ارائه کننده در تاک بپیوندید تنها کافی است با ما تماس بگیرید.</p>
-      </div>
-    </section>
-    <section class="section section--white pt-0 section--pb-lg">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4" v-for="(provider, username) in providers.all">
-            <provider :username="username" :provider="provider"/>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
+	<div>
+		<section class="section section--dark section--gradient section--gradient-one" id="section1">
+			<navbar />
+			<div class="container text-center text-white pt-5">
+				<h1 class="display-3 mb-3">ارائه کنندگان</h1>
+				<p>شما هم می توانید به افراد ارائه کننده در تاک بپیوندید تنها کافی است با ما تماس بگیرید.</p>
+			</div>
+		</section>
+		<section class="section section--white pt-0 section--pb-lg">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4" v-for="(provider, username) in providers.all" :key="username">
+						<provider :username="username" :provider="provider" />
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
-  import {mapState} from 'vuex';
-  import Navbar from "~/components/layouts/Navbar";
-  import Provider from "~/components/Provider";
+import { mapState } from "vuex";
+import Provider from "~/components/Provider";
+import Navbar from "~/components/layouts/Navbar";
 
-  export default {
-    name: "providers",
-    components: {Provider, Navbar},
-    computed: mapState(['providers']),
-    head() {
-      return {
-        title: 'ارائه کنندگان',
-      }
-    },
-  }
+export default {
+	name: "providers",
+	components: { Provider, Navbar },
+	computed: mapState(["providers"]),
+	head() {
+		return {
+			title: "ارائه کنندگان"
+		};
+	}
+};
 </script>
 
 <style scoped lang="scss">
+#section1 {
+	p {
+		font-size: 1.2rem;
+	}
 
-  #section1 {
-    p {
-      font-size: 1.2rem;
-    }
-
-    &:before {
-      background-image: url(/img/backgrounds/waves-1.svg), linear-gradient(326deg, #ffb964 20%, #421a82 92%);
-    }
-  }
+	&:before {
+		background-image: url(~assets/svg/backgrounds/waves-1.svg),
+			linear-gradient(326deg, #ffb964 20%, #421a82 92%);
+	}
+}
 </style>
